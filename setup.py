b9730cba40930fb9eca5eb3135e827d3b6011e15
Cheng Li <scrappedprince.li@gmail.com>
2016-08-17 14:05:47 +0800

    modified setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='FactorModel',
     version='0.1.0',
-    packages=[''],
+    packages=['FactorModel'],
     url='https://coding.net/u/wegamekinglc/p/FactorModels',
     license='',
     author='cheng.li',
