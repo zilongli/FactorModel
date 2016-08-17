b27095b9a22cc32835d81cdd8f31825d73e9c5bb
Cheng Li <scrappedprince.li@gmail.com>
2016-08-17 14:14:48 +0800

    update setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+from pip.req import parse_requirements
+
+requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+install_reqs = parse_requirements(requirements_path, session=False)
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='FactorModel',
@@ -8,5 +15,6 @@ setup(
     license='',
     author='cheng.li',
     author_email='wegamekinglc@hotmail.com',
-    description=''
+    description='',
+    install_requires=reqs
 )
