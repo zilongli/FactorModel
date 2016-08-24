babdbe98052e714db0498627b99839478d3bf8d8
cheng li <wegamekinglc@hotmail.com>
2016-08-24 12:59:10 +0800

    fix ubuntu dynamic library loading error.

--- FactorModel/optimizer.py
+++ FactorModel/optimizer.py
@@ -17,8 +17,11 @@ if sys.platform == "win32":
     _ = CDLL(alglib_dll_path)
     dll_handle = CDLL(optimizer_dll_path)
 else:
+    alglib_dll_path = os.path.join(dir_name, 'lib/libalglib.so')
     optimizer_dll_path = os.path.join(dir_name, 'lib/liboptimizer.so')
-    dll_handle = CDLL(optimizer_dll_path)
+
+_ = CDLL(alglib_dll_path)
+dll_handle = CDLL(optimizer_dll_path)
 
 
 def set_stop_condition(epsg,
