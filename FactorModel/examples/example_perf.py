eb54856e80422c57aecb49983d76ce34db5a678c
Cheng Li <scrappedprince.li@gmail.com>
2016-09-06 17:58:40 +0800

    update performance attributers

--- FactorModel/examples/example_perf.py
+++ FactorModel/examples/example_perf.py
@@ -4,12 +4,13 @@ Created on 2016-9-5
 
 @author: cheng.li
 """
-import datetime as dt
-import pandas as pd
-import numpy as np
+
 from pandas.io.excel import ExcelWriter
+from matplotlib import pyplot as plt
 from FactorModel.performance import PerfAttributeLOO
 from FactorModel.performance import PerfAttributeAOI
+from FactorModel.performance import PerfAttributeFocusLOO
+from FactorModel.performance import PerfAttributeFocusAOI
 from FactorModel.portcalc import ERRankPortCalc
 from FactorModel.schedule import Scheduler
 from FactorModel.ermodel import ERModelTrainer
@@ -30,7 +31,7 @@ trainer = ERModelTrainer(250, 1, 10)
 trainer.train_models(factor_names, env.source_data)#, train_dates=['2010-02-01'])
 cov_model = CovModel(env)
 port_calc = ERRankPortCalc(100, 101)
-scheduler = Scheduler(env, 'daily')
+scheduler = Scheduler(env, 'weekly')
 simulator = Simulator(env, trainer, cov_model, scheduler, port_calc)
 analyser = PnLAnalyser()
 
@@ -40,22 +41,13 @@ df2 = simulator.simulate()
 df1 = df1.loc[df2.index[0]:, :]
 df1[df2.columns] = df2
 
-from matplotlib import pyplot as plt
-
-attributer = PerfAttributeLOO()
-attributer.analysis(trainer, scheduler, port_calc, df1)
-
-attributer2 = PerfAttributeAOI()
-attributer2.analysis(trainer, scheduler, port_calc, df1)
-
-attributer.plot()
-plt.title('Performance Attribution (Leave One Out)')
-
-attributer2.plot()
-plt.title('Performance Attribution (Add One In)')
+with ExcelWriter('result_weekly.xlsx') as f:
+    for cls in [PerfAttributeLOO, PerfAttributeAOI, PerfAttributeFocusLOO, PerfAttributeFocusAOI]:
+        cls_name = cls.__name__
+        attributer = cls()
+        attributer.analysis(trainer, scheduler, port_calc, df1)
+        attributer.plot()
+        plt.title('Performance Attribution ' + '(' + cls_name + ')')
+        attributer.report.to_excel(f, cls_name)
 
 plt.show()
-
-with ExcelWriter('result2.xlsx') as wb:
-    attributer.report.to_excel(wb, 'loo')
-    attributer2.report.to_excel(wb, 'aoi')
--- FactorModel/performance.py
+++ FactorModel/performance.py
@@ -10,6 +10,7 @@ import numpy as np
 from FactorModel.schedule import Scheduler
 from FactorModel.portcalc import PortCalc
 from FactorModel.ermodel import ERModelTrainer
+from FactorModel.facts import BENCHMARK
 
 
 class PerfAttributeBase(metaclass=abc.ABCMeta):
@@ -37,6 +38,32 @@ class PerfAttributeBase(metaclass=abc.ABCMeta):
                    er_model,
                    port_calc):
         pass
+
+    def save_data(self,
+                  codes,
+                  calc_date,
+                  apply_date,
+                  today_holding,
+                  p_table,
+                  p_matrix,
+                  evolved_bm,
+                  total_pnl,
+                  factor_pnl,
+                  factor_names):
+        if self.report.empty:
+            num_cols = [s + '_num' for s in ['total'] + list(factor_names)]
+            weights_cols = [s + '_weight' for s in [BENCHMARK, 'total'] + list(factor_names)]
+            self.report = pd.DataFrame(columns=['calcDate', 'total'] + list(factor_names) + num_cols + weights_cols)
+        num_total = np.sum(today_holding != 0)
+        num_factors = np.sum(p_matrix != 0, axis=0)
+        bm_weight = np.sum(evolved_bm)
+        total_weight = np.sum(today_holding)
+        factor_weight = np.sum(p_matrix, axis=0)
+        self.report.loc[apply_date] = [calc_date, total_pnl] \
+                                      + list(factor_pnl) \
+                                      + [num_total] \
+                                      + list(num_factors) \
+                                      + [bm_weight, total_weight] + list(factor_weight)
 
     def analysis(self,
                  er_trainer: ERModelTrainer,
@@ -68,25 +95,28 @@ class PerfAttributeBase(metaclass=abc.ABCMeta):
                 cashes = 1. - evolved_new_table.sum()
                 evolved_new_table = evolved_new_table.multiply(1. + returns, axis=0)
                 evolved_new_table /= cashes + evolved_new_table.sum()
-                total_pnl = np.dot(today_holding - evolved_bm, returns)
+                total_pnl = np.dot(today_holding, returns) \
+                            - np.sum(today_holding) / np.sum(evolved_bm) * np.dot(evolved_bm, returns)
                 p_matrix = self._evolve(codes, today_holding, evolved_bm, evolved_new_table)
-                factor_pnl = returns @ p_matrix
+                factor_pnl = returns @ p_matrix \
+                             - np.sum(p_matrix, axis=0) / np.sum(evolved_bm) * np.dot(evolved_bm, returns)
                 self.p_table = evolved_new_table
-                self.report.loc[apply_date] = [calc_date, total_pnl] + list(factor_pnl)
             else:
                 evolved_preholding = this_data['evolvedPreHolding'].values
                 pre_holding = pd.DataFrame(evolved_preholding, index=codes, columns=['todayHolding'])
                 factor_values = this_data[factor_names]
                 er_model = er_trainer.fetch_model(apply_date)['model']
 
-                total_pnl = np.dot(today_holding - evolved_bm, returns)
+                total_pnl = np.dot(today_holding, returns) \
+                            - np.sum(today_holding) / np.sum(evolved_bm) * np.dot(evolved_bm, returns)
                 p_holding, p_matrix = self._rebalance(codes, today_holding, pre_holding, evolved_bm, factor_values, factor_names, er_model, port_calc)
-                factor_pnl = returns @ p_matrix
+                factor_pnl = returns @ p_matrix \
+                             - np.sum(p_matrix, axis=0) / np.sum(evolved_bm) * np.dot(evolved_bm, returns)
                 self.p_table = pd.DataFrame(p_holding, index=codes, columns=factor_names)
-                self.report.loc[apply_date] = [calc_date, total_pnl] + list(factor_pnl)
+            self.save_data(codes, calc_date, apply_date, today_holding, self.p_table, p_matrix, evolved_bm, total_pnl, factor_pnl, factor_names)
 
     def plot(self):
-        self.report[self.report.columns[1:]].cumsum().plot()
+        self.report[self.report.columns[1:5]].cumsum().plot()
 
 
 class PerfAttributeLOO(PerfAttributeBase):
@@ -137,9 +167,7 @@ class PerfAttributeAOI(PerfAttributeBase):
                 evolved_bm,
                 evolved_new_table):
         evolved_factor_p = evolved_new_table.values
-        tmp = evolved_bm.copy()
-        tmp.shape = -1, 1
-        return evolved_factor_p - tmp
+        return evolved_factor_p
 
     def _rebalance(self,
                    codes,
@@ -159,6 +187,87 @@ class PerfAttributeAOI(PerfAttributeBase):
             er_table = pd.DataFrame(er, index=codes, columns=['er'])
             res = port_calc.trade(er_table, pre_holding)
             p_holding[:, i] = res['todayHolding'].values
-        tmp = evolved_bm.copy()
+        return p_holding, p_holding
+
+
+class PerfAttributeFocusLOO(PerfAttributeBase):
+
+    def __init__(self):
+        super().__init__()
+
+    def _evolve(self,
+                codes,
+                today_holding,
+                evolved_bm,
+                evolved_new_table):
+        null_assets = np.array(today_holding == 0)
+        evolved_factor_p = evolved_new_table.values.copy()
+        evolved_factor_p[null_assets, :] = 0.
+        tmp = today_holding.copy()
         tmp.shape = -1, 1
-        return p_holding, p_holding - tmp
+        return tmp - evolved_factor_p
+
+    def _rebalance(self,
+                   codes,
+                   today_holding,
+                   pre_holding,
+                   evolved_bm,
+                   factor_values,
+                   factor_names,
+                   er_model,
+                   port_calc):
+        null_assets = np.array(today_holding == 0)
+        p_holding = np.zeros((len(codes), len(factor_names)), dtype=float)
+        for i, factor in enumerate(factor_names):
+            tb_copy = factor_values.copy(deep=True)
+            tb_copy[factor] = 0.
+            er = er_model.calculate_er(tb_copy)
+            er_table = pd.DataFrame(er, index=codes, columns=['er'])
+            res = port_calc.trade(er_table, pre_holding)
+            p_holding[:, i] = res['todayHolding'].values
+        filtered_p_holding = p_holding.copy()
+        filtered_p_holding[null_assets, :] = 0.
+        tmp = today_holding.copy()
+        tmp.shape = -1, 1
+        return p_holding, tmp - filtered_p_holding
+
+
+class PerfAttributeFocusAOI(PerfAttributeBase):
+
+    def __init__(self):
+        super().__init__()
+
+    def _evolve(self,
+                codes,
+                today_holding,
+                evolved_bm,
+                evolved_new_table):
+        null_assets = np.array(today_holding == 0)
+        evolved_factor_p = evolved_new_table.values.copy()
+        evolved_factor_p[null_assets, :] = 0.
+        return evolved_factor_p
+
+    def _rebalance(self,
+                   codes,
+                   today_holding,
+                   pre_holding,
+                   evolved_bm,
+                   factor_values,
+                   factor_names,
+                   er_model,
+                   port_calc):
+        null_assets = np.array(today_holding == 0)
+        p_holding = np.zeros((len(codes), len(factor_names)), dtype=float)
+        for i, factor in enumerate(factor_names):
+            tb_copy = factor_values.copy(deep=True)
+            tb_copy.loc[:, :] = 0.
+            tb_copy[factor] = factor_values[factor]
+            er = er_model.calculate_er(tb_copy)
+            er_table = pd.DataFrame(er, index=codes, columns=['er'])
+            res = port_calc.trade(er_table, pre_holding)
+            p_holding[:, i] = res['todayHolding'].values
+        filtered_p_holding = p_holding.copy()
+        filtered_p_holding[null_assets, :] = 0.
+        return p_holding, filtered_p_holding
+
+
