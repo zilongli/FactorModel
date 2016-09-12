d27a94f13b191fea98f7546935239a50ec96feab
Cheng Li <scrappedprince.li@gmail.com>
2016-09-12 18:15:19 +0800

    to make notional consistent

--- FactorModel/analysers.py
+++ FactorModel/analysers.py
@@ -23,9 +23,10 @@ class PnLAnalyser(object):
         for date in all_dates:
             returns = data.loc[date, ['nextReturn1day', 'evolvedBMWeight']]
             weights = data.loc[date, 'todayHolding']
-            pre_weights = data.loc[date, 'preHolding']
+            pre_weights = data.loc[date, 'evolvedPreHolding']
+            bm_weights = returns['evolvedBMWeight']
             this_day_return = returns['nextReturn1day'].values.T \
-                @ (weights.values - returns['evolvedBMWeight'].values)
+                @ (weights.values - np.sum(weights.values) / np.sum(bm_weights.values) * bm_weights.values)
             this_day_tc = np.sum(np.abs(weights.values - pre_weights.values)) \
                 * 0.002
             everyday_return.append(this_day_return)
