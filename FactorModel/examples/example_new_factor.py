9622a1f28bc58c41cfb17d880dfa22a70de1a1d7
Cheng Li <scrappedprince.li@gmail.com>
2016-09-23 18:40:35 +0800

    added one more example

--- FactorModel/examples/example_new_factor.py
+++ FactorModel/examples/example_new_factor.py
@@ -17,6 +17,8 @@ from FactorModel.analysers import PnLAnalyser
 from FactorModel.regulator import Regulator
 from FactorModel.facts import INDUSTRY_LIST
 from matplotlib import pyplot as plt
+import seaborn as sns
+sns.set_style('ticks')
 
 env = MSSQLProvider(
     '10.63.6.219',
@@ -28,7 +30,7 @@ factor_name = 'factor1'
 
 env.load_data('2014-01-01', '2015-12-31', None)
 
-external_data = pd.read_csv('/home/wegamekinglc/Downloads/factors20160921.csv',
+external_data = pd.read_csv('Z:/personal/jiangkai/factors20160921.csv',
                             parse_dates=[0],
                             header=0,
                             names=['applyDate', 'code', factor_name])
@@ -36,7 +38,7 @@ env.append(external_data)
 trainer = ERModelTrainer(250, 1, 5)
 trainer.train_models([factor_name], env.source_data)
 cov_model = CovModel(env)
-port_calc = ERRankPortCalc(in_threshold=100, out_threshold=101)
+port_calc = ERRankPortCalc(in_threshold=100, out_threshold=250)
 #port_calc = MeanVariancePortCalc('cost_budget', cost_budget=2e-4)
 scheduler = Scheduler(env, 'daily')
 constrinats_builder = Regulator(INDUSTRY_LIST)
