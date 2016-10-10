b2930eb9b7bc2fbcc8219c01760090cd28ad2104
Cheng Li <scrappedprince.li@gmail.com>
2016-10-10 17:09:33 +0800

    added config for start trading

--- FactorModel/schedule.py
+++ FactorModel/schedule.py
@@ -5,6 +5,7 @@ Created on 2016-9-2
 @author: cheng.li
 """
 
+import numpy as np
 from FactorModel.providers import Provider
 
 
@@ -12,9 +13,15 @@ class Scheduler(object):
 
     def __init__(self,
                  provider: Provider,
-                 freq='daily'):
+                 freq='daily',
+                 start_date=None):
         self.date_table = provider.date_table.copy(deep=True)
         self.freq = freq
+        if start_date:
+            self.start_date = np.datetime64(start_date)
+        else:
+            self.start_date = np.datetiem64('1970-01-01')
+
         if self.freq == 'biweekly':
             week_ends = \
                 self.date_table['eow'][self.date_table.eow == 1].astype(int)
@@ -24,20 +31,23 @@ class Scheduler(object):
             self.date_table.fillna(0, inplace=True)
 
     def is_rebalance(self, date):
-        if self.freq == 'daily':
-            return True
-        elif self.freq == 'weekly':
-            try:
-                return self.date_table.loc[date, 'eow'] == 1
-            except KeyError:
-                return False
-        elif self.freq == 'monthly':
-            try:
-                return self.date_table.loc[date, 'eom'] == 1
-            except KeyError:
-                return False
-        elif self.freq == 'biweekly':
-            try:
-                return self.date_table.loc[date, 'beow'] == 1
-            except KeyError:
-                return False
+        if date >= self.start_date:
+            if self.freq == 'daily':
+                return True
+            elif self.freq == 'weekly':
+                try:
+                    return self.date_table.loc[date, 'eow'] == 1
+                except KeyError:
+                    return False
+            elif self.freq == 'monthly':
+                try:
+                    return self.date_table.loc[date, 'eom'] == 1
+                except KeyError:
+                    return False
+            elif self.freq == 'biweekly':
+                try:
+                    return self.date_table.loc[date, 'beow'] == 1
+                except KeyError:
+                    return False
+        else:
+            return False
